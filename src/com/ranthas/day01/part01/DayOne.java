a4e63ddb5d878595359a584aa2c45470b87a367f
Ranthas90 <rubenreal1990@gmail.com>
2019-12-07 20:02:51 +0000

    Initial commit with day one done!

---
+++ src/com/ranthas/day01/part01/DayOne.java
@@ -0,0 +1,38 @@
+package com.ranthas.day01.part01;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class DayOne {
+
+    private static final String DATA_INPUT_FILE = "src/com/ranthas/day01/data-input.aoc";
+
+    public static void main(String[] args) {
+
+        try {
+            long fuelRequiredSum =
+                    readAllLinesFromFile(DATA_INPUT_FILE)
+                            .stream()
+                            .map(stringValue -> calculateFuelRequirement(Long.parseLong(stringValue)))
+                            .mapToLong(Long::longValue)
+                            .sum();
+
+            System.out.println(fuelRequiredSum);
+
+        } catch (IOException e) {
+            System.err.print("Error en la lectura del fichero!!!!");
+            e.printStackTrace();
+        }
+    }
+
+    private static Long calculateFuelRequirement(long mass) {
+        return (mass / 3L) - 2;
+    }
+
+    private static List<String> readAllLinesFromFile(String filePath) throws IOException {
+        return Files.readAllLines(Paths.get(filePath));
+    }
+}
